6b5748891eef79016292061a330ffa79df7ff1b4
omar-merhebi <116896797+omar-merhebi@users.noreply.github.com>
2024-06-04 23:51:25 -0400

    dataset splits

--- tensorflow/src/train.py
+++ tensorflow/src/train.py
@@ -1,10 +1,12 @@
 import logging
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 import wandb as wb
 
 from omegaconf import DictConfig
 from pathlib import Path
+from sklearn.model_selection._split import train_test_split
 from typing import Union, Tuple
 
 from .datasets import WayneRPEDataset
@@ -27,12 +29,20 @@ log = logging.getLogger(__name__)
 
 
 def train(cfg: DictConfig) -> None:
+    """
+    Run model training. 
+    Args:
+        cfg (DictConfig): The full hydra config.
+    """
+    
     # // TODO Finish train
+    
     # Check the config
     _check_train_cfg(cfg)
 
     # Create datasets
-
+    # // TODO: Create datasets
+    
     # Init Wandb
     run = wb.init(
         project=cfg.wandb.project,
@@ -63,14 +73,43 @@ def train(cfg: DictConfig) -> None:
     run.finish()
 
 
-def _create_splits(labels: Union[str, Path],
-                   splits: dict,
-                   random_seed: int = 416) -> Tuple[np.ndarray]:
-    # // TODO: create splits
-    pass
+def _create_splits(labels_csv: Union[str, Path],
+                   splits: DictConfig,
+                   random_seed: int = 416) -> Tuple[np.ndarray, 
+                                                    np.ndarray,
+                                                    np.ndarray]:
+    """
+    Creates train-test-val splits from the train configuration.
+    Args:
+        labels_csv (Union[str, Path]): path to the labels csv
+        splits (dict): splits configuration
+        random_seed (int, optional): Random seed. Defaults to 416.
 
+    Returns:
+        Tuple[np.ndarray, np.ndarray, np.ndarray]: tuple of arrays
+        indicating the indices present in the train, test, val sets.
+    """
+    
+    labels_csv = pd.read_csv(labels_csv)
+    
+    train_idx, test_idx = train_test_split(np.arange(len(labels_csv)),
+                                           train_size=splits.train,
+                                           random_state=random_seed)
+    
+    test_idx, val_idx = train_test_split(
+        test_idx,
+        train_size=splits.train / (splits.val + splits.test),
+        random_state=random_seed)
+    
+    return train_idx, test_idx, val_idx
+    
 
 def _check_train_cfg(cfg: DictConfig) -> None:
+    """
+    Validates the training configuration
+    Args:
+        cfg (DictConfig): the input configuration
+    """
 
     metrics = cfg.mode.metrics
     metric_names = cfg.mode.metric_names

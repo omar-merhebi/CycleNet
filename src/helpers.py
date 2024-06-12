0deb469c7dbca3d7e0c45e1438b9ee653d9a1e7c
omar-merhebi <116896797+omar-merhebi@users.noreply.github.com>
2024-06-12 11:58:16 -0400

    restructure

--- src/helpers.py
+++ src/helpers.py
@@ -1,13 +1,14 @@
-import json
+import hydra
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
 import tensorflow as tf
+import wandb as wb
 
 from datetime import datetime
 from collections.abc import Mapping
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 from pathlib import Path
 from PIL import Image
 from typing import Union, Optional
@@ -28,133 +29,50 @@ class ConfigError(Exception):
         super().__init__(self.message)
 
 
-def _get_dataset_length(labels_csv: Union[str, Path]) -> int:
+def init_sweep(config: DictConfig,
+               sweep_config: Union[str, Path]):
+
+    sweep_config = Path(sweep_config)
+
+    sweep_config = OmegaConf.load(sweep_config)
+    sweep_config = OmegaConf.to_container(sweep_config)
+
+    sweep_id = wb.sweep(sweep_config, project=config.wandb.project)
+
+    _freeze_config(config, fname=sweep_id)
+
+    return sweep_id
+
+
+def update_config(original, new_params):
+    for param, new_val, in new_params.items():
+        keys = param.split('.')
+        current = original
+        for key in keys[:-1]:
+            current = current.setdefault(key, {})
+        current[keys[-1]] = new_val
+
+
+def default_sweep_configs(config):
     """
-    Checks the length of the dataset provided
+    Updates some of the config parameters for sweeps.
     Args:
-        labels_csv (Union[str, Path]): The CSV file containing the
-        dataset labels.
-
-    Returns:
-        int: The length of the dataset.
-    """
-    labels_csv = str(labels_csv)
-
-    labels_csv = pd.read_csv(labels_csv)
-
-    return len(labels_csv)
-
-
-def _check_leakage(**kwargs: Optional[np.ndarray]):
-    """
-    Takes np.ndarrays for dataset indicies and finds if there is an
-    intersection for any of them.
+        config (DictConfig): the original config.
     """
 
-    sets = {
-        name: set(idx) if idx is not None else set()
-        for name, idx in kwargs.items()
+    to_change = {
+        "mode": {"early_stopping": False}
+        "save_model"
     }
-
-    names = list(sets.keys())
-
-    print('Checking for data leakage...')
-
-    leaks = False
-    for i in range(len(names)):
-        for j in range(i+1, len(names)):
-            intersection = sets[names[i]] & sets[names[j]]
-
-            if not intersection == set():
-                leaks = True
-                print('Data Leakage detected in dataset indices:'
-                      f'{names[i]} and {names[j]}')
-
-    if not leaks:
-        print('No data leakage found.')
-
-    print('Finished data leakage check.')
+    
+    update_config(config, to_change)
 
 
-def _get_splits(dataset_args: DictConfig) -> list:
-    """
-    Looks at the datasets and their arguments and gets a list of splits.
-    Args:
-        dataset_args (DictConfig): from cfg.dataset.args
+def _freeze_config(config: DictConfig, fname: str):
+    save_path = PROJECT_PATH / 'frozen_configs' / f'{fname}.yaml'
+    save_path.parent.mkdir(parents=True, exist_ok=True)
 
-    Returns:
-        list: list of split values
-    """
-
-    return [dataset_args[arg]['split'] for arg in dataset_args]
-
-
-def generate_save_path(parent_dir: Union[str, Path],
-                       model_name: str,
-                       file_name: str,
-                       extension: str = '.tf',
-                       include_time: bool = False) -> str:
-    """
-    Gets a save path for the results/models.
-    Args:
-        parent_dir (Union[str, Path]): The directory where all such results are
-        stored. ex. save_models/ or results/
-        model_name (str): the name of the model (will be a parent directory)
-        file_name (str): the name of the file to save (without the extension)
-        extension (str, optional): the extension to add to the file name.
-        Defaults to '.tf'.
-        include_time (bool, optional): whether to include the time in the
-        name of the file. Defaults to False.
-
-    Returns:
-        str: the output filepath as a string.
-    """
-
-    today = TODAY.strftime('%Y-%m-%d')
-    now = TODAY.strftime('%H-%M-%S')
-
-    parent_dir = Path(parent_dir)
-    extension = extension.replace('.', '')
-
-    save_path = parent_dir / model_name / today
-
-    save_path.mkdir(parents=True, exist_ok=True)
-
-    if include_time:
-        file_name = f'{file_name}_{now}.{extension}'
-
-    else:
-        file_name = f'{file_name}.{extension}'
-
-    save_path = save_path / file_name
-
-    return save_path
-
-
-def log_cfg(cfg: DictConfig) -> None:
-    """
-    Logs the Hydra configuration.
-    Args:
-        cfg (DictConfig): Hydra configuration dict.
-    """
-
-    pretty_cfg = json.dumps(cfg, indent=4)
-
-    print('----------------------Loaded Config----------------------')
-    print(f'{pretty_cfg}')
-
-
-def log_env_details(cpus: int, gpus: int, total_mem: int) -> None:
-    """
-    Logs environment details at level INFO.
-    Args:
-        cpus (int): Number of CPUs
-        gpus (int): Number of GPUs
-        total_mem (int): Amount of Memory in MB
-    """
-    print(f"CPUs:\t{cpus}")
-    print(f"GPUs:\t{gpus}")
-    print(f"Available Memory:\t{total_mem} MB")
+    OmegaConf.save(config, save_path)
 
 
 def test_gpu(force_gpu: bool = False) -> None:
@@ -173,41 +91,6 @@ def test_gpu(force_gpu: bool = False) -> None:
                 "force_gpu is set to True.")
         else:
             print("Failed to detect GPU. Using CPU instead.")
-
-
-def convert_tensor_to_image(img_tensor: tf.Tensor) -> Image:
-    """
-    Convert a TensorFlow tensor to a Image.
-    Args:
-        img_tensor (tf.Tensor): The image tensor
-
-    Returns:
-        Image: Output image
-    """
-
-    if img_tensor.dtype != tf.float32:
-        img_tensor = tf.cast(img_tensor, tf.float32)
-
-    img_arr = img_tensor.numpy()
-    img_arr = img_arr.squeeze()
-
-    if len(img_arr.shape) != 2:
-        raise ConfigError(
-            'Attempted conversion of image to tensor with non-2D shape.\n'
-            f'Invalid image shape: {img_arr.shape}.'
-        )
-
-    img_min = img_arr.min()
-    img_max = img_arr.max()
-
-    if img_min == img_max:
-        return Image.fromarray(np.zeros(img_arr.shape))
-
-    normalized_img = CMAP((img_arr - img_min) / (img_max - img_min))
-    image = (normalized_img[:, :, :3]*255).astype(np.uint8)
-    image = Image.fromarray(image)
-
-    return image
 
 
 def get_resource_allocation():
@@ -248,110 +131,3 @@ def _determine_slurm():
     Determines if the current environment is a SLURM environment.
     """
     return os.environ.get('SLURM_JOB_ID') is not None
-
-
-def check_config(cfg: DictConfig):
-    """
-    Check the configuration object for any missing or invalid values.
-
-    Args:
-        cfg (DictConfig): The configuration object
-    """
-
-    if not cfg.dataset:
-        raise ConfigError('No dataset configuarion found.')
-
-    if not cfg.dataset.data_dir:
-        raise ConfigError('No data directory found.')
-
-    try:
-        filters_given = len(cfg.model.filters)
-
-    except TypeError:
-        raise ConfigError(
-            'Filters configuration for the model should be a list,'
-            f'not {type(cfg.model.filters)}'
-        )
-
-    if filters_given != cfg.model.num_conv_layers + 1:
-        raise ConfigError(
-            "Must provide the number of filters to use for each layer,"
-            "i.e., len(filters) == num_conv_layers in model config. Got:\n"
-            f"Number of filters:\t{filters_given}\n"
-            f"Number of Conv Layers:\t{cfg.model.num_conv_layers}"
-        )
-
-    try:
-        neurons_given = len(cfg.model.dense_neurons)
-
-    except TypeError:
-        raise ConfigError(
-            "Dense layer neuron configuration for the model should be a list,"
-            f"not {type(cfg.model.dense_neurons)}"
-        )
-
-    if neurons_given != cfg.model.num_dense_layers:
-        raise ConfigError(
-            "Must provide the number of filters to use for each layer,"
-            "i.e., len(filters) == num_conv_layers in model config. Got:\n"
-            f"Number of filters:\t{neurons_given}\n"
-            f"Number of Conv Layers:\t{cfg.model.num_dense_layers}")
-
-
-def convert_paths(cfg: DictConfig) -> DictConfig:
-    """
-    Convert all paths in the configuration object to Posix paths.
-    Args:
-        cfg (DictConfig): The configuration object
-
-    Returns:
-        DictConfig: The configuration object with all
-        paths converted to Posix paths.
-    """
-
-    if isinstance(cfg, Mapping):
-        for key, value in cfg.items():
-            if isinstance(value, str):
-                cfg[key] = _to_posix(value)
-
-            elif isinstance(value, list):
-                cfg[key] = [_to_posix(v) if isinstance(v, str) and
-                            ('/' in v or '\\' in v)
-                            else v for v in value]
-
-            else:
-                convert_paths(value)
-
-    elif isinstance(cfg, list):
-        for i, value in enumerate(cfg):
-            if isinstance(value, str):
-                if '/' in value or '\\' in value:
-                    cfg[i] = _to_posix(value)
-
-            elif isinstance(value, Mapping):
-                cfg[i] = _to_posix(value)
-
-    return cfg
-
-
-def _to_posix(path: Union[str, Path], project_path: Path = PROJECT_PATH):
-    """
-    Convert a path to a Posix path.
-    Args:
-        path (Union[str, Path]): The path to convert
-        project_path (Path, optional): project directory.
-        Defaults to PROJECT_PATH.
-
-    Returns:
-        Path: a path object
-    """
-
-    if '/' in path or '\\' in path:
-        new_path = Path(path)
-
-        if not new_path.is_absolute() and '~' not in path:
-            new_path = project_path / new_path
-
-        return new_path
-
-    return path
--- src/train.py
+++ src/train.py
@@ -1,6 +1,5 @@
 import numpy as np
 import tensorflow as tf
-import time
 import wandb as wb
 
 from datetime import datetime
@@ -9,10 +8,10 @@ import pandas as pd
 from pathlib import Path
 import pprint
 from sklearn.model_selection._split import train_test_split
-from tqdm import tqdm
 
 from . import model_builder as mb
 from . import datasets as d
+from . import helpers as h
 
 NOW = datetime.now()
 DATE = NOW.strftime('%Y-%m-%d')
@@ -30,34 +29,49 @@ class WandbMetricsLogger(tf.keras.callbacks.Callback):
             'val_accuracy': logs.get('val_categorical_accuracy')})
 
 
-def run_train(cfg, save_model=False):
+def run_sweep():
+    wb.init()
+    sweep_id = wb.run.sweep_id
 
-    train_ds, val_ds, test_ds = _load_datasets(cfg)
+    config = OmegaConf.load(
+        h.PROJECT_PATH / 'frozen_configs' / f'{sweep_id}.yaml')
 
-    cfg_dict = OmegaConf.to_container(cfg, resolve=True)
+    sweep_config = wb.config
+
+    h.update_config(config, sweep_config)
+    h.default_sweep_configs(config)
+    print(config)
+    # train(config)
+
+
+def train(config, save_model=False):
+
+    train_ds, val_ds, test_ds = _load_datasets(config)
+
+    cfg_dict = OmegaConf.to_container(config, resolve=True)
     pprint.pp(cfg_dict)
 
     wb.init(config=cfg_dict,
-            **cfg.wandb)
+            **config.wandb)
 
     print(f'Train input shape: {train_ds[0][0][0].shape}')
-    model = mb.build_model(cfg.model,
+    model = mb.build_model(config.model,
                            input_shape=train_ds[0][0][0].shape,
                            num_classes=3)
 
     model.compile(optimizer=mb._get_optimizer(
-                        cfg.mode.optimizer, **cfg.mode.optimizer_args),
-                  loss=mb._get_loss(cfg.mode.loss),
+                        config.mode.optimizer, **config.mode.optimizer_args),
+                  loss=mb._get_loss(config.mode.loss),
                   metrics=[mb._get_metric(metric)
-                           for metric in cfg.mode.metrics])
+                           for metric in config.mode.metrics])
 
     model.summary()
 
     callbacks = [WandbMetricsLogger()]
 
     if save_model:
-        model_save_path = cfg.model_save_path
-        model_save_path = Path(model_save_path) / cfg.wandb.name / DATE
+        model_save_path = config.model_save_path
+        model_save_path = Path(model_save_path) / config.wandb.name / DATE
         model_save_path.mkdir(exist_ok=True, parents=True)
         model_save_path = model_save_path / f'best_model_{TIME}.keras'
         print(f'Saving model to:\t{model_save_path}')
@@ -70,22 +84,20 @@ def run_train(cfg, save_model=False):
 
         callbacks.append(mc_save)
 
-    if cfg.mode.early_stopping.enabled:
+    if config.mode.early_stopping.enabled:
         early_stop = tf.keras.callbacks.EarlyStopping(
             monitor='val_loss',
-            patience=cfg.mode.early_stopping.patience
+            patience=config.mode.early_stopping.patience
         )
 
         callbacks.append(early_stop)
 
-    hist = model.fit(train_ds, epochs=cfg.mode.epochs,
+    hist = model.fit(train_ds, epochs=config.mode.epochs,
                      validation_data=val_ds,
                      callbacks=callbacks)
 
     history = hist.history
 
-    print('Waiting 30s before ending...')
-    time.sleep(30)
     wb.finish()
 
 
@@ -124,91 +136,6 @@ def _load_datasets(cfg):
     )
 
     return train_ds, val_ds, test_ds
-
-
-def train(train_dataset, val_dataset, model, optimizer, loss_fn,
-          train_acc_metric, val_acc_metric, model_save_path, save_model=False,
-          epochs=10, log_step=200, val_log_step=50):
-
-    best_val_loss = 10000
-
-    for epoch in range(epochs):
-        print(f'Start of epoch {epoch}')
-
-        train_loss = []
-        val_loss = []
-
-        # Iterate over the batches of the dataset
-        print('Training step:')
-        for step, (x_batch_train, y_batch_train, log_batch_train) in tqdm(enumerate(train_dataset),
-                                                                          total=len(train_dataset)):
-            if x_batch_train is None:
-                break
-
-            loss_value = train_step(x_batch_train, y_batch_train,
-                                    model, optimizer, loss_fn,
-                                    train_acc_metric)
-
-            train_loss.append(float(loss_value))
-
-        # Validation step
-        print('Validation step:')
-        for step, (x_batch_val, y_batch_val, log_batch_val) in tqdm(enumerate(val_dataset),
-                                                                    total=len(val_dataset)):
-
-            if x_batch_val is None:
-                break
-
-            val_loss_value = test_step(x_batch_val, y_batch_val,
-                                       model, loss_fn, val_acc_metric)
-
-            val_loss.append(float(val_loss_value))
-
-        if np.mean(val_loss) < best_val_loss and save_model:
-            best_val_loss = val_loss_value
-            model.save(model_save_path)
-
-        # Display training metrics at end of each epoch
-        train_acc = train_acc_metric.result()
-        print(f'Training loss over epoch: {np.mean(train_loss)}')
-        print(f'Training acc over epoch: {float(train_acc):.4f}')
-
-        val_acc = val_acc_metric.result()
-        print(f'Validation loss over epoch: {np.mean(val_loss)}')
-        print(f'Validation acc over epoch: {float(val_acc):.4f}')
-
-        # Reset metrics
-        train_acc_metric.reset_state()
-        val_acc_metric.reset_state()
-
-        wb.log({'epochs': epoch,
-                'loss': np.mean(train_loss),
-                'acc': float(train_acc),
-                'val_loss': np.mean(val_loss),
-                'val_acc': float(val_acc)})
-
-
-def train_step(x, y, model, optimizer,
-               loss_fn, train_acc_metric):
-
-    with tf.GradientTape() as tape:
-        logits = model(x, training=True)
-        loss_val = loss_fn(y, logits)
-
-    grads = tape.gradient(loss_val, model.trainable_weights)
-    optimizer.apply_gradients(zip(grads, model.trainable_weights))
-
-    train_acc_metric.update_state(y, logits)
-
-    return loss_val
-
-
-def test_step(x, y, model, loss_fn, val_acc_metric):
-    val_logits = model(x, training=False)
-    loss_val = loss_fn(y, val_logits)
-    val_acc_metric.update_state(y, val_logits)
-
-    return loss_val
 
 
 def _create_data_splits(splits, data_len, random_seed):

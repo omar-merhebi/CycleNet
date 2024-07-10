3a33dd1712c6f5cd22262ba8308d64d5c2919557
omar-merhebi <116896797+omar-merhebi@users.noreply.github.com>
2024-07-10 13:28:34 -0400

    started new model building method

--- src/model_builder.py
+++ src/model_builder.py
@@ -3,9 +3,11 @@ import tensorflow as tf
 from functools import partial
 from omegaconf import DictConfig
 
-from tensorflow.keras.models import Model  # type: ignore
-from tensorflow.keras.layers import Dense, Dropout, Conv2D, \
-    AveragePooling2D, MaxPool2D, Flatten, Input  # type: ignore
+from tensorflow import keras
+from keras.models import Model  # type: ignore
+from keras.layers import Dense, Dropout, Conv2D, AveragePooling2D, MaxPooling2D, Flatten, Input  # type: ignore
+
+from icecream import ic
 
 OPTIMIZERS = {
     'adam': tf.keras.optimizers.Adam,
@@ -35,6 +37,110 @@ def build_model(cfg: DictConfig, **kwargs) -> tf.keras.Model:
 
     if architecture in ['cnn', 'conv']:
         model = build_cnn(cfg, **kwargs)
+
+    elif architecture == 'alexnet':
+        model = build_alexnet(cfg, **kwargs)
+
+    return model
+
+
+def build_alexnet(cfg: DictConfig,
+                  input_shape: tf.TensorShape = tf.TensorShape([100, 100, 55]),
+                  num_classes: int = 3) -> tf.keras.Model:
+    """
+    Builds AlexNet-like model for class prediction
+    Args:
+        cfg (DictConfig): The model config
+        input_shape (tf.TensorShape, optional): The shape of the image inputs
+        to the CNN. Defaults to tf.TensorShape([100, 100, 55]).
+        num_classes (int, optional): The number of classes to output.
+        Defaults to 3.
+
+    Returns:
+        tf.keras.Model: The AlexNet model.
+    """
+
+    if type(input_shape) is list:
+        input_shape = tf.TensorShape(input_shape)
+
+    layers = [layer.lower() for layer in cfg.layers]
+
+    for layer in layers:
+        assert layer in ['conv2d', 'maxpooling2d', 'averagepooling2d',
+                         'dense', 'flatten']
+
+    num_conv_layers = len([layer for layer in layers if 'conv' in layer])
+    num_pool_layers = len([layer for layer in layers if 'pool' in layer])
+    num_dense_layers = len([layer for layer in layers if 'dense' in layer])
+
+    assert len(cfg.filters) == len(cfg.conv_kernels) == len(cfg.conv_pads) == \
+        len(cfg.conv_strides) == num_conv_layers
+
+    assert len(cfg.pool_sizes) == len(cfg.pool_pads) == \
+        len(cfg.pool_strides) == num_pool_layers
+
+    assert len(cfg.dense_neurons) == len(cfg.dropouts) == num_dense_layers
+
+    inputs = Input(shape=input_shape, name="InputLayer")
+    x = inputs
+
+    conv_layer = 0
+    pool_layer = 0
+    flatten_layer = 0
+    dense_layer = 0
+
+    print(layers)
+
+    for layer in layers:
+        if layer == 'conv2d':
+            x = Conv2D(filters=cfg.filters[conv_layer],
+                       kernel_size=cfg.conv_kernels[conv_layer],
+                       activation=cfg.activation,
+                       strides=cfg.conv_strides[conv_layer],
+                       padding=cfg.conv_pads[conv_layer],
+                       kernel_initializer=cfg.initializer,
+                       name=f'Conv{conv_layer}')(x)
+            conv_layer += 1
+
+        elif layer == 'maxpooling2d':
+            x = MaxPooling2D(pool_size=cfg.pool_sizes[pool_layer],
+                             strides=cfg.pool_strides[pool_layer],
+                             padding=cfg.pool_pads[pool_layer],
+                             name=f'MaxPool{pool_layer}')(x)
+
+            pool_layer += 1
+
+        elif layer == 'averagepooling2d':
+            x = AveragePooling2D(pool_size=cfg.pool_sizes[pool_layer],
+                                 strides=cfg.pool_strides[pool_layer],
+                                 padding=cfg.pool_pads[pool_layer],
+                                 name=f'AveragePool{pool_layer}')(x)
+
+            pool_layer += 1
+
+        elif layer == 'flatten':
+            x = Flatten(name=f'Flatten{flatten_layer}')(x)
+
+            flatten_layer += 1
+
+        elif layer == 'dense':
+            x = Dense(cfg.dense_neurons[dense_layer],
+                      activation=cfg.activation,
+                      name=f'Dense{dense_layer}')(x)
+
+            x = Dropout(cfg.dropouts[dense_layer],
+                        name=f'Dropout{dense_layer}')(x)
+
+            dense_layer += 1
+
+        else:
+            raise RuntimeError('Weird Error')
+
+    class_out = Dense(num_classes, activation='softmax',
+                      name='OutputLayer')(x)
+
+    model = Model(inputs=inputs, outputs=class_out)
+    print(model.summary())
 
     return model
 
@@ -150,7 +256,7 @@ def _get_pool_partial(pool_type: str = "avg", **kwargs):
         return partial(AveragePooling2D, **kwargs)
 
     if pool_type in ['max', 'maximum']:
-        return partial(MaxPool2D, **kwargs)
+        return partial(MaxPooling2D, **kwargs)
 
     raise ValueError(f'Invalid pool type: {pool_type}')
 
